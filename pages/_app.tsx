09f91e0a88438cdc807676ddc0f167175ba7e69e
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-05-01 03:22:42 +0000

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { DefaultSeo, NextSeo } from 'next-seo';
+import { DefaultSeo } from 'next-seo';
 import theme from '../styles/theme';
 import seo from '../next-seo.config';
 import Head from 'next/head';
